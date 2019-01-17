5108426d1785dfb74283a0680a395e0a7fc6ba58
marwajomaa <marwajomaa4@gmail.com>
2019-01-17 13:16:33 +0200

    passing function into video list items via props

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import "./App.css"
 
-const VideoItem = ({video})=>{
+const VideoItem = ({video, onSelectedVideo})=>{
 const { title } = video.snippet;
 const { url } = video.snippet.thumbnails.medium;
   return(
-    <li className="list_item" >
+    <li className="list_item" onClick= {()=>onSelectedVideo(video)} >
       <div className="vedio_item">
       <div className="video_img">
        <img className="video_image" src={ url } alt={title} />
