241959e1551fc3b8a1690573f0e365f1dbdd04c2
marwajomaa <marwajomaa4@gmail.com>
2019-01-17 13:15:48 +0200

    passing props into video list

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "./App.css"
 import VideoItem from './video_list_item'
 
-const VideoList = ({videos})=> {
+const VideoList = ({videos, onSelectedVideo})=> {
   if(!videos){
     return 'loading'
 }
@@ -10,7 +10,7 @@ const VideoList = ({videos})=> {
   return(
     <div className="vedio_list">
      <ul>
-    {videos.map(video=> <VideoItem key={video.etag} video={video} />)}
+    {videos.map(video=> <VideoItem key={video.etag} video={video} onSelectedVideo={onSelectedVideo}/>)}
      </ul>
     </div>
   )
