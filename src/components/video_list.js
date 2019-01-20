03bd2ea93534a4ddf5a83ff09f536803bfdffabc
Marwa Jomaa <34250163+marwajomaa@users.noreply.github.com>
2019-01-20 02:17:40 +0200

    Update video_list.js

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "./App.css"
 import VideoItem from './video_list_item'
 
-const VideoList = ({videos, onSelectedVideo})=> {
+const VideoList = ({ videos, onSelectedVideo })=> {
   if(!videos){
     return 'loading'
 }
@@ -10,7 +10,7 @@ const VideoList = ({videos, onSelectedVideo})=> {
   return(
     <div className="vedio_list">
      <ul>
-      {videos.map(video=> <VideoItem key={video.etag} video={video} onSelectedVideo={onSelectedVideo}/>)}
+      {videos.map(video=> <VideoItem key={ video.etag } video={ video } onSelectedVideo={ onSelectedVideo } />)}
      </ul>
     </div>
   )
