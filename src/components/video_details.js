2eaaf190735a2654d7d3b40ca2f2275daf54cfc0
Marwa Jomaa <34250163+marwajomaa@users.noreply.github.com>
2019-01-20 02:20:21 +0200

    Update video_details.js

--- src/components/video_details.js
+++ src/components/video_details.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./App.css";
 
-const VideoDetails = ({video})=>{
+const VideoDetails = ({ video })=>{
  if(!video){
    return 'loading'
  }
@@ -12,11 +12,11 @@ const VideoDetails = ({video})=>{
   return(
     <div className="vedio_details">
     <div>
-      <iframe src={url} className="iframe"/>
+      <iframe src={ url } className="iframe" />
      </div>
      <div className="video_descriptions">
-      <div className="video_title">{title}</div>
-      <div className="video_des"><p>{description}</p></div>
+      <div className="video_title">{ title }</div>
+      <div className="video_des"><p>{ description }</p></div>
      </div>
     </div>
   )
