75ea095198fe5cb9310b45407ecf6770d7eae011
Marwa Jomaa <34250163+marwajomaa@users.noreply.github.com>
2019-01-20 02:21:06 +0200

    Update video_list_item.js

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import "./App.css"
 
-const VideoItem = ({video, onSelectedVideo})=>{
+const VideoItem = ({ video, onSelectedVideo })=>{
 const { title } = video.snippet;
 const { url } = video.snippet.thumbnails.medium;
   return(
     <li className="list_item" onClick= {()=>onSelectedVideo(video)} >
       <div className="vedio_item">
       <div className="video_img">
-       <img className="video_image" src={ url } alt={title} />
+       <img className="video_image" src={ url } alt={ title } />
       </div>
       <div className="video_title">
-       <p>{title}</p>
+       <p>{ title }</p>
       </div>
       </div>
     </li>
